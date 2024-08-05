baaec12c61af6650544ddf8d257fb325eeeae196
Daniel Earwicker <dan@earwicker.com>
2024-08-05 23:47:57 +0100

    Actually run tests

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ node apply-version.js
 rm -rf bin
 rm -rf obj
 dotnet build --configuration Release
-dotnet test --configuration Release Parquet.Producers/Parquet.Producers.csproj
+dotnet test Parquet.Producers.Tests/Parquet.Producers.Tests.csproj
 
 export VER=`cat .version`
 dotnet nuget push Parquet.Producers/nupkg/Parquet.Producers.$VER.nupkg -k $NUGET_API_KEY -s https://api.nuget.org/v3/index.json
